4d1d08f76fbd8b4189bd594a224183a131e3d9cf
Julian Flake <flake@uni-koblenz.de>
2024-02-26 21:44:40 +0100

    readded astah

--- nutguix/packages/astah.scm
+++ nutguix/packages/astah.scm
@@ -1,22 +1,21 @@
-(define-module (nutguix packages astah))
+;;; Copyright © 2023 Julian Flake <flake@uni-koblenz.de>
 
-;; guix package --install-from-file=astah.scm
-;; guix install  -L . astah-professional
+(define-module (nutguix packages astah)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system copy)
+  #:use-module ((nonguix licenses) #:prefix license:)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages java))
 
+;; Getting the binary:
 ;;
 ;; guix download https://cdn.change-vision.com/files/astah-professional_9.0.0.1778f1-0_all.deb
 ;; ar x wga0hq1569ki48amhhv7rs5zk8a2rr0j-astah-professional_9.0.0.1778f1-0_all.deb
 ;; xz -d data.tar.xz
 
-(use-modules (guix packages)
-             (guix download)
-             (guix build-system copy)
-             (guix licenses)
-	     (gnu packages base)
-	     (gnu packages java))
-
 (define-public astah-professional
-  
+
   (package
     (name "astah-professional")
     (version "9.0.0.1778f1")
@@ -29,7 +28,7 @@
 	 "05n4vprifx3lsk5cp626zzc487vg558g4jp6cpdgx4gi710lz1f5"))))
     (build-system copy-build-system)
     (inputs (list binutils))
-;;    (propagated-inputs (list openjdk))
+    ;;    (propagated-inputs (list openjdk))
     (arguments
      '(#:install-plan
        '(("usr/" "/"))
@@ -37,11 +36,11 @@
        (modify-phases %standard-phases
 	 (replace 'unpack
            (lambda* (#:key source #:allow-other-keys)
-             ;; Unpack the tarball into 'umoci' instead of "runc-${version}".
              (invoke "ar" "x" source)
 	     (invoke "tar" "xf" "data.tar.xz"))))))
     (synopsis "Astah Professional")
     (description
      "Full-Featured Software Modeling Tool for creating UML, ER Diagrams, DFD, Flowchart and more to create a clear understanding of your software design among teams.Easy-to-use UML2.x modeler")
     (home-page "http://astah.net/products/astah-professional")
-    (license agpl1)))
+    (license (license:nonfree "file://lib/astah_professional/AstahLicenseAgreement-e.txt"))))
+
