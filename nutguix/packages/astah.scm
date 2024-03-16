89a2e68556fb302ea281e3d61ece332e5e644c87
Julian Flake <flake@uni-koblenz.de>
2024-03-16 15:56:30 +0100

    fixed astah download url

--- nutguix/packages/astah.scm
+++ nutguix/packages/astah.scm
@@ -19,30 +19,30 @@
 (define-public astah-professional
 
   (package
-   (name "astah-professional")
-   (version "9.2.0.0248cd")
-   (source
-    (origin
-     (method url-fetch)
-     (uri "https://members.change-vision.com/files/_EfTycaWjuxKxIiqxCcIZ68vJNAY22QdY/astah_professional/9_2_0/astah-professional_9.2.0.0248cd-0_all.deb")
-     (sha256
-      (base32
-       "1nqccj4dh53ll5drxf4pj74ab52c31zw8hbs6kw7g51x7fi3wy59"))))
-   (build-system copy-build-system)
-   (inputs (list binutils))
-   (propagated-inputs (list icedtea))
-   (arguments
-    '(#:install-plan
-      '(("usr/" "/"))
-      #:phases
-      (modify-phases %standard-phases
-		     (replace 'unpack
-			      (lambda* (#:key source #:allow-other-keys)
-				(invoke "ar" "x" source)
-				(invoke "tar" "xf" "data.tar.xz"))))))
-   (synopsis "Astah Professional")
-   (description
-    "Full-Featured Software Modeling Tool for creating UML, ER Diagrams, DFD, Flowchart and more to create a clear understanding of your software design among teams.Easy-to-use UML2.x modeler")
-   (home-page "http://astah.net/products/astah-professional")
-   (license (license:nonfree "file://lib/astah_professional/AstahLicenseAgreement-e.txt"))))
+    (name "astah-professional")
+    (version "9.2.0.0248cd")
+    (source
+     (origin
+       (method url-fetch)
+       (uri "https://cdn.change-vision.com/files/astah-professional_9.2.0.0248cd-0_all.deb")
+       (sha256
+	(base32
+	 "1nqccj4dh53ll5drxf4pj74ab52c31zw8hbs6kw7g51x7fi3wy59"))))
+    (build-system copy-build-system)
+    (inputs (list binutils))
+    (propagated-inputs (list icedtea))
+    (arguments
+     '(#:install-plan
+       '(("usr/" "/"))
+       #:phases
+       (modify-phases %standard-phases
+	 (replace 'unpack
+	   (lambda* (#:key source #:allow-other-keys)
+	     (invoke "ar" "x" source)
+	     (invoke "tar" "xf" "data.tar.xz"))))))
+    (synopsis "Astah Professional")
+    (description
+     "Full-Featured Software Modeling Tool for creating UML, ER Diagrams, DFD, Flowchart and more to create a clear understanding of your software design among teams.Easy-to-use UML2.x modeler")
+    (home-page "http://astah.net/products/astah-professional")
+    (license (license:nonfree "file://lib/astah_professional/AstahLicenseAgreement-e.txt"))))
 
